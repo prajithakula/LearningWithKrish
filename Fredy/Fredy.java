b22c479d4c91582ac98e365b98126c53e3030a97
Prajitha Kulendran <47469917+prajithakula@users.noreply.github.com>
2022-01-01 17:16:05 +0530

    Add files via upload

--- Fredy/Fredy.java
+++ Fredy/Fredy.java
@@ -2,36 +2,48 @@ import java.util.Scanner;
 class Fredy{
 	public static void main(String[] args){
 		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the distance");
-		double length = scanner.nextDouble();
 		
-	int jumpCount=0;
-	int gap = 0;
-	double totalTime=0;
+		try{
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter the distance");
+			double length = scanner.nextDouble();
+			
+			int jumpCount=0;
+			int gap = 0;
+			double totalTime=0;
+			if(length > 0){
+					while(length > 0){
+					if(gap==0 && length > 0){
+						length = length - 5;
+						jumpCount++;
+						totalTime = totalTime + 1;
+						gap++;
+					}
+					if(gap==1 && length > 0){
+						length = length - 3;
+						jumpCount++;
+						totalTime = totalTime + 2;
+						gap++;
+					}
+					if(gap==2 && length > 0){
+						length = length - 1;
+						jumpCount++;
+						totalTime = totalTime + 3.5;
+						gap=0;
+					}
+				
+				}
+			System.out.println("Fredy's total time is taken for the distance :"+ totalTime);
+			System.out.println("Fredy's total jump count is for the distance :"+ jumpCount);
+			}
+			else {
+				System.out.println("Invalid Distance value");
+			}
+		}
+		catch(Exception ex){
+				System.out.println("Invalid input");
+		}
 	
-	while(length > 0){
-		if(gap==0 && length > 0){
-			length = length - 5;
-			jumpCount++;
-			totalTime = totalTime + 1;
-			gap++;
-		}
-		if(gap==1 && length > 0){
-			length = length - 3;
-			jumpCount++;
-			totalTime = totalTime + 2;
-			gap++;
-		}
-		if(gap==2 && length > 0){
-			length = length - 1;
-			jumpCount++;
-			totalTime = totalTime + 3.5;
-			gap=0;
-		}
-		
-	}
-	System.out.println("Fredy's total time is taken for the distance :"+ totalTime);
-	System.out.println("Fredy's total jump count is for the distance :"+ jumpCount);
+	
 	}
 }
